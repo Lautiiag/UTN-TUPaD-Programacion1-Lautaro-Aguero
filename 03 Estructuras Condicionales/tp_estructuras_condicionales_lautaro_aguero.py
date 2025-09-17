7ca8002cd6a41111cdee835273806b28fd328dbe
Lautiiag <lautiiag179@gmail.com>
2025-09-17 01:46:31 -0300

    Varios ejercicios y comentarios

--- 03 Estructuras Condicionales/tp_estructuras_condicionales_lautaro_aguero.py
+++ 03 Estructuras Condicionales/tp_estructuras_condicionales_lautaro_aguero.py
@@ -1,6 +1,28 @@
 "Trabajo Práctico Condicionales Unidad 03 Lautaro Agustín Agüero"
-#1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
-#deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”
+# 1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años
+# deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”
 edad = int(input("Ingrese su edad: "))
 if edad > 18:
     print("Es mayor de edad")
+else:
+    pass
+
+
+# 2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+# mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+# mensaje “Desaprobado”.
+nota_usuario=float(input("Ingrese su nota: "))
+if nota_usuario >= 6:
+    print("Aprobado")
+else:
+    print("Desaprovado")
+
+
+# 3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+# número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+# contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+# operador de módulo (%) en Python para evaluar si un número es par o impar.
+
+
+num_usuario=int(input("Ingrese un número par: "))
+if num_usuario % == 0:
