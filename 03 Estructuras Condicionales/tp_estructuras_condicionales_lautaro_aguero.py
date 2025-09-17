2a4275768a54dd54209b436c75cf619bf5ae6794
Lautiiag <lautiiag179@gmail.com>
2025-09-17 01:58:24 -0300

    Ejercicio 04 y consigna 05

--- 03 Estructuras Condicionales/tp_estructuras_condicionales_lautaro_aguero.py
+++ 03 Estructuras Condicionales/tp_estructuras_condicionales_lautaro_aguero.py
@@ -29,3 +29,32 @@ if num_usuario % 2 == 0:
     print("Ha ingresado un número par")
 else:
     print("Por favor, ingrese un número par")
+
+
+# 4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+# siguientes categorías pertenece:
+# ● Niño/a: menor de 12 años.
+# ● Adolescente: mayor o igual que 12 años y menor que 18 años.
+# ● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+# ● Adulto/a: mayor o igual que 30 años
+
+
+edad_usuario=int(input("Ingrese su edad: "))
+if edad_usuario < 12:
+    print("Niño/a.")
+elif edad_usuario >= 12 and edad_usuario < 18:
+    print("Adolescente.")
+elif edad_usuario >= 18 and edad_usuario < 30:
+    print("Adulto/a joven.")
+elif edad_usuario >= 30:
+    print("Adulto/a.")
+
+
+# 5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+# (incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+# pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+# pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+# de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+# como una lista o un string.
+
+
