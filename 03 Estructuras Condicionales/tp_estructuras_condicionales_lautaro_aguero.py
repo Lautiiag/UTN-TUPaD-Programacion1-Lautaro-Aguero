fa28bf78affef80bc17f9dec195f418097c90ae7
Lautiiag <lautiiag179@gmail.com>
2025-09-17 03:03:12 -0300

    Inicio Ejercicio 08

--- 03 Estructuras Condicionales/tp_estructuras_condicionales_lautaro_aguero.py
+++ 03 Estructuras Condicionales/tp_estructuras_condicionales_lautaro_aguero.py
@@ -96,3 +96,16 @@ else:
     print(frase)
 
 
+# 8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3
+# dependiendo de la opción que desee:
+# 1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+# 2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+# 3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+# El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el
+# usuario e imprimir el resultado por pantalla.
+
+nombre=input("Ingrese su nombre")
+opcion=int(input("Ingrese una opción:"))
+print("1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.")
+print("2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.")
+print("3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.")
