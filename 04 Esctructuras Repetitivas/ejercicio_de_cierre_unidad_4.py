c5c6a3884c45d95027dfd326412a10b50e93fa36
Lautiiag <lautiiag179@gmail.com>
2025-09-18 05:52:39 -0300

    ejercicio 4

--- 04 Esctructuras Repetitivas/ejercicio_de_cierre_unidad_4.py
+++ 04 Esctructuras Repetitivas/ejercicio_de_cierre_unidad_4.py
@@ -25,3 +25,20 @@ for i in range(minimo+1,maximo):
     suma= suma + i
 print("La suma es: ", suma)
 
+
+# 4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
+# secuencia. El programa debe detenerse y mostrar el total acumulado cuando el usuario ingrese
+# un 0
+
+num1=1
+suma1=0
+while True:
+    if num1 == 0:
+        break
+    num1=int(input("Ingrese un número entero para sumar: "))
+    suma1= suma1 + num1
+print(suma1)
+
+
+
+
