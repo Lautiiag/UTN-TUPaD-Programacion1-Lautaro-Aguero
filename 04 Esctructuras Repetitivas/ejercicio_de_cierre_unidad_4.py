be459ed4bc4ddb90e1223ef9abfc09dcedd45eba
Lautiiag <lautiiag179@gmail.com>
2025-09-18 06:01:43 -0300

    ejercicio 05

--- 04 Esctructuras Repetitivas/ejercicio_de_cierre_unidad_4.py
+++ 04 Esctructuras Repetitivas/ejercicio_de_cierre_unidad_4.py
@@ -40,5 +40,18 @@ while True:
 print(suma1)
 
 
+# 5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
+# programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
+
+import random
+
+numero_aleatorio=random.randint(0,9)
+intentos=0
+while True:
+    num2=int(input("Ingrese un número: "))
+    intentos += 1
+    if num2 == numero_aleatorio:
+        break
+print("Intentos realizados: ", intentos)
 
 
--- pruebas_personales.py
+++ pruebas_personales.py
@@ -1,16 +1,15 @@
 "PRUEBAS"
 #----------------------------------#
+import random
 
+# 5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
+# programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
 
-# 4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
-# secuencia. El programa debe detenerse y mostrar el total acumulado cuando el usuario ingrese
-# un 0
-
-num1=1
-suma1=0
+numero_aleatorio=random.randint(0,9)
+intentos=0
 while True:
-    if num1 == 0:
+    num2=int(input("Ingrese un número: "))
+    intentos += 1
+    if num2 == numero_aleatorio:
         break
-    num1=int(input("Ingrese un número entero para sumar: "))
-    suma1= suma1 + num1
-print(suma1)
+print("Intentos realizados: ", intentos)
