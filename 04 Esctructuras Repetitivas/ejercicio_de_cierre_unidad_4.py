65b01d3950120d4eb432507ebce7f280236449fa
Lautiiag <lautiiag179@gmail.com>
2025-09-18 03:10:59 -0300

    punto 1

---
+++ 04 Esctructuras Repetitivas/ejercicio_de_cierre_unidad_4.py
@@ -0,0 +1,12 @@
+"Ejercicio de cierre de unidad 4"
+# 1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+# (incluyendo ambos extremos), en orden creciente, mostrando un número por línea
+# PARA ESTE EJERCICIO SE UTILIZA UNA VARIABLE PARA INDICAR EL NUMERO MAXIMO
+
+num_max=int(input("Ingrese el número entero máximo: "))
+for i in range(0,num_max+1):
+    print(i)
+
+
+# 2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+# dígitos que contiene.
