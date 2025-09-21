624fe2d8f3d6335f08cd00b61b85c9e0056f3337
Lautiiag <lautiiag179@gmail.com>
2025-09-21 19:34:56 -0300

    Creacion archivo matrices

---
+++ 00 Personales/matrices.py
@@ -0,0 +1,35 @@
+"Matrices"
+
+# imprimir:
+# 0,1,2,3,4
+# 5,6,7,8,9
+# 10,11,12,13,14
+# 15,16,17,18,19
+
+matriz=[
+[0,1,2,3,4,],
+[5,6,7,8,9],
+[10,11,12,13,14],
+[15,16,17,18,19],
+]
+
+cant_filas=len(matriz)
+cant_columnas=len(matriz[0])
+
+for filas in range(cant_filas):
+    for columnas in range(cant_columnas):
+        print(matriz[filas][columnas], end=" ")
+    print()
+
+
+
+# Mismo ejercicio pero con variables mas claras
+# cantidad_filas=len(matriz)
+# cantidad_columnas=len(matriz[0])
+
+# for indice_fila in range(cantidad_filas):
+#     for indice_columna in range(cantidad_columnas):
+#         print(matriz[indice_fila][indice_columna], end=" ")
+#     print()
+
+# print(matriz[2][3])
--- Auxiliar.py
+++ Auxiliar.py
@@ -1,38 +1,12 @@
 "PRUEBAS"
 #----------------------------------#
-# imprimir:
-# 0,1,2,3,4
-# 5,6,7,8,9
-# 10,11,12,13,14
-# 15,16,17,18,19
+# 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
+# penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
+# indexing con números negativos!
+print("Ejercicio 02")
 
-
-# cant_filas=len(matriz)
-# cant_columnas=len(matriz[0])
-
-# for filas in range(cant_filas):
-#     for columnas in range(cant_columnas):
-#         print(matriz[filas][columnas], end=" ")
-#     print()
-
-
-
-# matriz=[
-# [0,1,2,3,4,],
-# [5,6,7,8,9],
-# [10,11,12,13,14],
-# [15,16,17,18,19],
-# ]
-
-# cantidad_filas=len(matriz)
-# cantidad_columnas=len(matriz[0])
-
-# for indice_fila in range(cantidad_filas):
-#     for indice_columna in range(cantidad_columnas):
-#         print(matriz[indice_fila][indice_columna], end=" ")
-#     print()
-
-# print(matriz[2][3])
+random=["UTN", "Lauti",1,True,2.15,"Último"]
+print(random[:-1])
 
 
 
