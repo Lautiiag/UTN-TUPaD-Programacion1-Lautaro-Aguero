1831235789c883d4970072429fe740bfd7e975f5
Lautiiag <lautiiag179@gmail.com>
2025-09-21 19:19:04 -0300

    creacion archivo actividad de cierre unidad 5

--- Auxiliar.py
+++ Auxiliar.py
@@ -1,10 +1,54 @@
 "PRUEBAS"
 #----------------------------------#
+# imprimir:
+# 0,1,2,3,4
+# 5,6,7,8,9
+# 10,11,12,13,14
+# 15,16,17,18,19
 
 
-# 10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
-# usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
-print("EJERCICIO 10")
+# cant_filas=len(matriz)
+# cant_columnas=len(matriz[0])
 
-numero_a_invertir=input("Ingrese un número entero: ")
-print(numero_a_invertir[::-1])
+# for filas in range(cant_filas):
+#     for columnas in range(cant_columnas):
+#         print(matriz[filas][columnas], end=" ")
+#     print()
+
+
+
+# matriz=[
+# [0,1,2,3,4,],
+# [5,6,7,8,9],
+# [10,11,12,13,14],
+# [15,16,17,18,19],
+# ]
+
+# cantidad_filas=len(matriz)
+# cantidad_columnas=len(matriz[0])
+
+# for indice_fila in range(cantidad_filas):
+#     for indice_columna in range(cantidad_columnas):
+#         print(matriz[indice_fila][indice_columna], end=" ")
+#     print()
+
+# print(matriz[2][3])
+
+lista1=[1,2,3]
+lista2=[4,5,6]
+print(lista1+lista2)
+print(lista1)
+print(lista2)
+
+
+
+
+
+
+
+
+
+
+
+
+
