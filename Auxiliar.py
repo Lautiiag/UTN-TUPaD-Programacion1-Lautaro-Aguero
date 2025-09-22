3b590cebf5f9708b73e502c50f9c6a49ce91c055
Lautiiag <lautiiag179@gmail.com>
2025-09-22 00:32:48 -0300

    opcion 1 crudo

--- Auxiliar.py
+++ Auxiliar.py
@@ -7,12 +7,9 @@ cupos=[]
 cant_especialidades=int(input("Ingrese cantidad de especialidades a añadir: "))
 for i in range(cant_especialidades):
     nueva_especialidad=input("Ingrese nueva especialidad: ").capitalize()
-    if nueva_especialidad is str: # Valida que sea string
-        if nueva_especialidad in especialidades: #Valida se está añadida
-            print("Esta especialidad ya fue añadida.")
-        else:
-            especialidades.append(nueva_especialidad)
+    if nueva_especialidad.isalpha(): # Valida que sea string
+        especialidades.append(nueva_especialidad)
     else:
-        while nueva_especialidad is not str:
+        while nueva_especialidad :
             nueva_especialidad= input("Ingrese una especialidad válida: ")
     print(especialidades)
--- Auxiliar2.py
+++ Auxiliar2.py
@@ -1,18 +1,4 @@
-"AUXILIAR 2"
-#----------------------------------#
-
-especialidades=[]
-cupos=[]
-
-cant_especialidades=int(input("Ingrese cantidad de especialidades a añadir: "))
-for i in range(cant_especialidades):
-    nueva_especialidad=input("Ingrese nueva especialidad: ").capitalize()
-    if nueva_especialidad is str: # Valida que sea string
         if nueva_especialidad in especialidades: #Valida se está añadida
             print("Esta especialidad ya fue añadida.")
         else:
-            especialidades.append(nueva_especialidad)
-    else:
-        while nueva_especialidad is not str:
-            nueva_especialidad= input("Ingrese una especialidad válida: ")
-    print(especialidades)
+            
