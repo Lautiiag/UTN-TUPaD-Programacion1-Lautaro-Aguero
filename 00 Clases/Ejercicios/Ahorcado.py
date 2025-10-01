90900d6339dc48bc175ecf088732bdf21f536f6b
Lautiiag <lautiiag179@gmail.com>
2025-10-01 09:32:01 -0300

    wtf ahorcado

--- 00 Clases/Ejercicios/Ahorcado.py
+++ 00 Clases/Ejercicios/Ahorcado.py
@@ -2,15 +2,20 @@
 import random
 
 
-def elegir_palabra(palabras):
+def eleccion_de_palabra(palabras):
     palabra_elegida=palabras[random.randrange(0,len(palabras))]
     return palabra_elegida
 
+def print_guiones(palabra_elegida):
+    for i in range(len(palabra_elegida)):
+        print("_", end=" ")
+
+
+
+
+
 
 palabras=("pera", "banana", "manzana", "naranja", "mandarina", "uva", "zapallo", "papa", "apio", "pepino", "durazno")
-palabra_elegida_afuera=elegir_palabra(palabras)
-print(palabra_elegida_afuera)
-
-print(len(palabra_elegida_afuera))
-
+palabra_elegida=eleccion_de_palabra(palabras)
+print(palabra_elegida)
 
