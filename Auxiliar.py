117de7313f76f660aa8d64edc76daa7071b0a703
Lautiiag <lautiiag179@gmail.com>
2025-10-01 08:50:34 -0300

    Ahorcado y otros

--- Auxiliar.py
+++ Auxiliar.py
@@ -1,14 +1,4 @@
 "AUXILIAR"
 #----------------------------------#
-especialidades=["espe1","espe2","espe3","espe4"]
-cupos=[1,2,3,4]
-
-
-print(especialidades)
-print(cupos)
-
-
-
-# Cuando mostras la lista
-for i in especialidades: # Recorre cada elemento de la lista
-    print(i.capitalize()) # Capitaliza cada elemento
+frase=input("INgrese frase: ").title()
+print(frase)
