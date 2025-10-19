abcb632ff44fec3429957be4f782db686641788a
Lautiiag <lautiiag179@gmail.com>
2025-10-19 01:51:58 -0300

    Ejercicio 6

--- 07 Datos Complejos/tp_estructura_de_datos_complejas.py
+++ 07 Datos Complejos/tp_estructura_de_datos_complejas.py
@@ -1,7 +1,20 @@
 "Tp Datos complejos"
-
+#------------------FUNCIONES
+#Ejercicio 4
 def agregar_elementos_a_diccionario(nombre_diccionario,cantidad_elementos,key,value):
     nombre_diccionario[key]=value
+#Ejercicio 6
+def sacar_promedio(notas_tupla): #Saca promedios
+    promedio=sum(notas_tupla) / len(notas_tupla)
+    return promedio
+def pedir_notas(cant_notas_funcion): #Pide la cantidad las notas para sacar promedio
+    notas=[]
+    for contador_notas in range(cant_notas_funcion):
+        nota=float(input(f"Ingrese nota {contador_notas+1}: "))
+        notas.append(nota) 
+    return tuple(notas)
+
+#------------------RESOLUCION DE PROBLEMAS
 
 # 1) Dado el diccionario precios_frutas
 #precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}
@@ -60,8 +73,15 @@ frase_lista=frase.split()
 frase_set=set(frase_lista)
 print(frase_set)
 
-
-
-
 # 6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas.
 # Luego, mostrá el promedio de cada alumno.
+# 6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas.
+# Luego, mostrá el promedio de cada alumno.
+print("Ejercicio 6")
+
+for i in range(3):
+    nombre = input(f"Ingrese nombre del alumno {i+1}: ")
+    notas = pedir_notas(3)
+    promedio = sacar_promedio(notas)
+    print(f"El promedio de {nombre} es de {round(promedio, 2)} puntos.\n")
+
--- Auxiliar.py
+++ Auxiliar.py
@@ -1,8 +1,21 @@
 "AUXILIAR"
 #----------------------------------#
 
-print("Ejercicio 5")
-frase=input("Ingrese frase para mostrar palabras únicas de la misma: ")
-frase_lista=frase.split()
-frase_set=set(frase_lista)
-print(frase_set)
+# 6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas.
+# Luego, mostrá el promedio de cada alumno.
+def sacar_promedio(notas_tupla): #Saca promedios
+    promedio=sum(notas_tupla) / len(notas_tupla)
+    return promedio
+def pedir_notas(cant_notas_funcion): #Pide la cantidad las notas para sacar promedio
+    notas=[]
+    for contador_notas in range(cant_notas_funcion):
+        nota=float(input(f"Ingrese nota {contador_notas+1}: "))
+        notas.append(nota) 
+    return tuple(notas)
+
+for i in range(3):
+    nombre = input(f"Ingrese nombre del alumno {i+1}: ")
+    notas = pedir_notas(3)
+    promedio = sacar_promedio(notas)
+    print(f"El promedio de {nombre} es de {round(promedio, 2)} puntos.\n")
+
